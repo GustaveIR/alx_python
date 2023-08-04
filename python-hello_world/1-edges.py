b8e88e0883549001954f44244d01a19fa9aca59f
GustaveIR <iraguhagustave@gmail.com>
2023-08-04 17:58:44 +0200

    python-hello_world

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,9 @@
-# File: 1-edges.py
+#!/usr/bin/python3
 
-word = "HolaHolberto"
-word_first_3, word_last_2, middle_word = word[:3], word[-2:], word[1:-1]
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
 
 print("First 3 letters:", word_first_3)
 print("Last 2 letters:", word_last_2)
