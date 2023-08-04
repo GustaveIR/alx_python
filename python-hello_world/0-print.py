845cff352b21a1e754d624964f8e4de9a7e7d78b
GustaveIR <iraguhagustave@gmail.com>
2023-08-04 17:19:00 +0200

    python-hello_world

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,4 +1,3 @@
 # File: 0-print.py
 
-words_to_print = ["Programming", "is", "like", "building", "a", "multilingual", "puzzle"]
-print(*words_to_print)
+print("\"Programming is like building a multilingual puzzle")
