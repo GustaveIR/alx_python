841a6eb2da516c5ef3992d01fc647c8ab85fcb9b
GustaveIR <iraguhagustave@gmail.com>
2023-08-05 23:23:26 +0200

    pyhton-hello_world

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import random
 
-number = random.randint(-10, 10)
+try:
+    number = random.randint(-10, 10)
 
-# Check if the number is an integer
-if isinstance(number, int):
     # Check if the number is positive, negative, or zero, and print the corresponding message
     if number > 0:
         print(f"{number} is positive")
@@ -12,5 +11,6 @@ if isinstance(number, int):
         print(f"{number} is zero")
     else:
         print(f"{number} is negative")
-else:
+
+except TypeError:
     print("Error: The generated number is not an integer.")
