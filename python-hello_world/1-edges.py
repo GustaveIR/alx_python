8adb80d6208c8b7e62d4f149a8f6eed4d97ef332
GustaveIR <iraguhagustave@gmail.com>
2023-08-05 22:58:01 +0200

    python-hello_world

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 
 word = "Holberton"
-
-# Extract the first 3 letters of the variable "word"
-word_first_3 = word[:3]
-
-# Extract the last 2 letters of the variable "word"
-word_last_2 = word[-2:]
-
-# Extract the middle word without the first and last letters
-middle_word = word[1:-1]
-
+word_first_3, word_last_2, middle_word = word[:3], word[-2:], word[1:-1]
 print("First 3 letters:", word_first_3)
 print("Last 2 letters:", word_last_2)
 print("Middle word:", middle_word)
