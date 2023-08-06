047d84255873755787efa4e3cf291a4dcdf8b233
GustaveIR <iraguhagustave@gmail.com>
2023-08-07 00:21:21 +0200

    python-functions

--- python-functions/python-coding/1-main.py
+++ python-functions/python-coding/1-main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 pow = __import__('python-functions.1-power').pow
 
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+result = pow(-98, -10)
+formatted_result = "{:.20f}".format(result)
+print(formatted_result)
