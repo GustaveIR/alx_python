1f520209ae4ed75e8c69b77afcce179724ab058d
GustaveIR <iraguhagustave@gmail.com>
2023-08-07 00:28:28 +0200

    python-functions

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,4 +1,9 @@
-#!/usr/bin/python3
-pow = __import__('python-functions.1-power').pow
-
-print(pow(-98, -10))
+def pow(a, b):
+    result = 1
+    if b >= 0:
+        for _ in range(b):
+            result *= a
+    else:
+        for _ in range(-b):
+            result /= a
+    return result
