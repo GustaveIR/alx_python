fc52cf2f191530acd15e65bb489cd408953fddcd
GustaveIR <iraguhagustave@gmail.com>
2023-08-06 13:15:18 +0200

    python-hello_world

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 import random
 
-try:
-    number = random.randint(-10, 10)
+number = random.randint(-10, 10)
 
-    # Check if the number is an integer
-    if isinstance(number, int):
-        # Check if the number is positive, negative, or zero, and print the corresponding message
-        if number > 0:
-            print(f"{number} is positive")
-        elif number == 0:
-            print(f"{number} is zero")
-        else:
-            print(f"{number} is negative")
+# Check if the number is an integer
+if isinstance(number, int):
+    # Check if the number is positive, negative, or zero, and print the corresponding message
+    if number > 0:
+        print(f"{number} is positive")
+    elif number == 0:
+        print(f"{number} is zero")
     else:
-        raise TypeError(f"Error: The generated number '{number}' is not an integer.")
-
-except TypeError as e:
-    print(str(e))  # Print the full error message as a string
+        print(f"{number} is negative")
+else:
+    print(f"Error: The generated number '{number}' is not an integer.")
