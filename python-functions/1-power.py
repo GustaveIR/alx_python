d7df793a670ac0e5cca3a5741d0a21ab22a5707e
GustaveIR <iraguhagustave@gmail.com>
2023-08-07 00:32:44 +0200

    python-functions

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,9 +1,13 @@
 def pow(a, b):
+    if b == 0:
+        return 1
+
     result = 1
-    if b >= 0:
+    if b > 0:
         for _ in range(b):
             result *= a
     else:
         for _ in range(-b):
             result /= a
+
     return result
