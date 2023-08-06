1eb5d7abd0596c1c2469e6e0911b4e1d48423dab
GustaveIR <iraguhagustave@gmail.com>
2023-08-06 13:24:01 +0200

    python-hello_world

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+
+# Get the last digit of the number using modulo 10
+last_digit = abs(number) % 10
+
+# Determine the appropriate suffix for the last digit
+if last_digit > 5:
+    suffix = "and is greater than 5"
+elif last_digit == 0:
+    suffix = "and is 0"
+else:
+    suffix = "and is less than 6 and not 0"
+
+# Print the result
+print(f"Last digit of {number} is {last_digit} {suffix}")
