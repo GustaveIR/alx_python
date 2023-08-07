39ae445f1398cf5a728647559cd9774dab385fa9
GustaveIR <iraguhagustave@gmail.com>
2023-08-07 18:12:31 +0200

    python-functions

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,5 +1,3 @@
-# 2-temperature.py
-
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * (5 / 9)
 
@@ -7,10 +5,5 @@ def convert_to_celsius(fahrenheit):
     if abs(celsius + 273.15) < 0.001:
         return -273.15
 
-    return celsius
-
-# Test cases
-print("{:.14f}".format(convert_to_celsius(100)))  # Output: 37.77777777777778
-print("{:.14f}".format(convert_to_celsius(-40)))  # Output: -40.00000000000001
-print("{:.14f}".format(convert_to_celsius(-459.67)))  # Output: -273.14999999999986
-print("{:.14f}".format(convert_to_celsius(32)))  # Output: 0.0
+    # Round to two decimal places for other values
+    return round(celsius, 2)
