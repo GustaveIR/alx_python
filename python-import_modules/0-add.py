5ab3bd47d874229dfe8a617e565285193c81e815
GustaveIR <iraguhagustave@gmail.com>
2023-08-07 19:35:12 +0200

    python-import_modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -12,30 +12,3 @@ result = add(a, b)
 
 # Print the result in the desired format
 print("{} + {} = {}".format(a, b, result))
-
-# Correct output - case: a = 1 and b = 2 FAKE add() => a - b
-print("Correct output - case: a = {} and b = {} FAKE add() => {} - {}".format(a, b, a, b))
-
-# Correct output - case: a = 10 and b = 30 FAKE add() => a - b
-a = 10
-b = 30
-result = add(a, b)
-print("Correct output - case: a = {} and b = {} FAKE add() => {} - {}".format(a, b, a, b))
-
-# Correct output - case: a = -10 and b = 30 FAKE add() => a - b
-a = -10
-b = 30
-result = add(a, b)
-print("Correct output - case: a = {} and b = {} FAKE add() => {} - {}".format(a, b, a, b))
-
-# Correct output - case: a = -10 and b = -30 FAKE add() => a - b
-a = -10
-b = -30
-result = add(a, b)
-print("Correct output - case: a = {} and b = {} FAKE add() => {} - {}".format(a, b, a, b))
-
-# Correct output - case: a = 5 and b = "H" FAKE add() => a - b , here are the errors , please help to fix them
-a = 5
-b = "H"
-result = add(a, b)
-print("Correct output - case: a = {} and b = {} FAKE add() => {} - {}".format(a, b, a, b))
