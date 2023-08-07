49dbf3a812a48643699c6ec535cdf7386f105b5c
GustaveIR <iraguhagustave@gmail.com>
2023-08-07 19:29:13 +0200

    python-import_modules

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+a = 1
+b = 2
+
+# Import the add function from add_0.py
+from add_0 import add
+
+# Calculate the result of the addition using the add function
+result = add(a, b)
+
+# Print the result in the desired format
+print("{} + {} = {}".format(a, b, result))
