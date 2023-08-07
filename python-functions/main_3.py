bb115d6d4389224b1ab7e4a3f9f96d7311f1873d
GustaveIR <iraguhagustave@gmail.com>
2023-08-07 12:46:49 +0200

    python-functions

--- python-functions/main_3.py
+++ python-functions/main_3.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+from decimal import Decimal, getcontext
 from python-functions.1-power import power
 
+# Set the precision to 20 decimal places
+getcontext().prec = 20
+
 result = power(-98, -10)
-formatted_result = "{:.18e}".format(result)
+formatted_result = format(Decimal(result), '.18e')
 print(formatted_result)
