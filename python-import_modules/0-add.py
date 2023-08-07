a425be60e6b8d180b11aeb8af210d30d40f4f2b2
GustaveIR <iraguhagustave@gmail.com>
2023-08-07 19:32:04 +0200

    python-import_modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,3 +1,6 @@
+# 0-add.py
+
+# Define the variables a and b
 a = 1
 b = 2
 
@@ -9,3 +12,30 @@ result = add(a, b)
 
 # Print the result in the desired format
 print("{} + {} = {}".format(a, b, result))
+
+# Correct output - case: a = 1 and b = 2 FAKE add() => a - b
+print("Correct output - case: a = {} and b = {} FAKE add() => {} - {}".format(a, b, a, b))
+
+# Correct output - case: a = 10 and b = 30 FAKE add() => a - b
+a = 10
+b = 30
+result = add(a, b)
+print("Correct output - case: a = {} and b = {} FAKE add() => {} - {}".format(a, b, a, b))
+
+# Correct output - case: a = -10 and b = 30 FAKE add() => a - b
+a = -10
+b = 30
+result = add(a, b)
+print("Correct output - case: a = {} and b = {} FAKE add() => {} - {}".format(a, b, a, b))
+
+# Correct output - case: a = -10 and b = -30 FAKE add() => a - b
+a = -10
+b = -30
+result = add(a, b)
+print("Correct output - case: a = {} and b = {} FAKE add() => {} - {}".format(a, b, a, b))
+
+# Correct output - case: a = 5 and b = "H" FAKE add() => a - b , here are the errors , please help to fix them
+a = 5
+b = "H"
+result = add(a, b)
+print("Correct output - case: a = {} and b = {} FAKE add() => {} - {}".format(a, b, a, b))
