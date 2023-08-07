dff23696b8b2e4149b8323ef927bac2aae9b90b0
GustaveIR <iraguhagustave@gmail.com>
2023-08-07 16:21:25 +0200

    python-functions

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,3 +1,8 @@
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * (5 / 9)
+
+    # Handle floating-point precision issue for -459.67
+    if abs(celsius + 273.15) < 0.001:
+        return -273.15
+
     return round(celsius, 2)
