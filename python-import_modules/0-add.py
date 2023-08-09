97904fcb6b8019132516a3d7e7bf2ac19685b4f4
GustaveIR <iraguhagustave@gmail.com>
2023-08-09 22:16:38 +0200

    python-import_modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,28 +1,8 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a - b
+from add_0 import add
 
 a = 1
 b = 2
-result = add(a, b)
-print("{} - {} => {}".format(a, b, result))
 
-a = 10
-b = 30
 result = add(a, b)
-print("{} - {} => {}".format(a, b, result))
-
-a = -10
-b = 30
-result = add(a, b)
-print("{} - {} => {}".format(a, b, result))
-
-a = -10
-b = -30
-result = add(a, b)
-print("{} - {} => {}".format(a, b, result))
-
-a = 5
-b = "H"
-result = add(a, b)
-print("{} - {} => {}".format(a, b, result))
+print("{} + {} = {}".format(a, b, result))
