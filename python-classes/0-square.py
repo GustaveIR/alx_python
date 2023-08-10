894dc15200b69d7ac15d72f75c7862439cc805d1
GustaveIR <iraguhagustave@gmail.com>
2023-08-10 23:02:48 +0200

    python-classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
+"""0-square.py: Defines a Square class with private instance attribute 'size'."""
 
 class Square:
-    """
-    A class that defines a square by a private instance attribute 'size'.
+    """Represents a square."""
 
-    Attributes:
-        size (int): The size of the square.
-    """
     def __init__(self, size):
-        """
-        Initializes a new Square instance.
-
-        Args:
-            size (int): The size of the square.
-        """
+        """Initialize a new Square instance."""
         self.__size = size
 
-# Additional test code
+# The following code is for testing purposes
 if __name__ == "__main__":
-    mysquare = Square(3)
-    print(type(mysquare))
-    print(mysquare.__dict__)
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
