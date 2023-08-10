b3f41096f5df27f1848d0f2412f7218bd18fbca1
GustaveIR <iraguhagustave@gmail.com>
2023-08-10 23:04:42 +0200

    python-classes

---
+++ python-classes/1-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""1-square.py: Defines a Square class with private instance attribute 'size' and size validation."""
+
+class Square:
+    """Represents a square."""
+    
+    def __init__(self, size=0):
+        """Initialize a new Square instance."""
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+# The following code is for testing purposes
+if __name__ == "__main__":
+    my_square_1 = Square(3)
+    print(type(my_square_1))
+    print(my_square_1.__dict__)
+
+    my_square_2 = Square()
+    print(type(my_square_2))
+    print(my_square_2.__dict__)
+
+    try:
+        print(my_square_1.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square_1.__size)
+    except Exception as e:
+        print(e)
+
+    try:
+        my_square_3 = Square("3")
+        print(type(my_square_3))
+        print(my_square_3.__dict__)
+    except Exception as e:
+        print(e)
+
+    try:
+        my_square_4 = Square(-89)
+        print(type(my_square_4))
+        print(my_square_4.__dict__)
+    except Exception as e:
+        print(e)
