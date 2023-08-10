1bf34595cecd0c1f58e37f41a6d1af0d0ff3125b
GustaveIR <iraguhagustave@gmail.com>
2023-08-11 00:28:55 +0200

    python-network_1

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Takes your GitHub credentials (username and password) and uses the GitHub API to display your id.
+Uses Basic Authentication with a personal access token as password.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        sys.exit("Usage: {} <username> <token>".format(sys.argv[0]))
+    
+    username = sys.argv[1]
+    token = sys.argv[2]
+    url = "https://api.github.com/user"
+
+    response = requests.get(url, auth=(username, token))
+    try:
+        json_response = response.json()
+        print(json_response.get("id"))
+    except ValueError:
+        print("None")
