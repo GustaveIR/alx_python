9a53aa03330379867ae19a92de827358ab205ed8
GustaveIR <iraguhagustave@gmail.com>
2023-09-29 19:01:09 +0200

    python-object_relational_mapping

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     
     username = sys.argv[1]
     token = sys.argv[2]
-    url = "https://api.github.com/user"
+    url = "https://api.github.com/GustaveIR"
 
     response = requests.get(url, auth=(username, token))
     try:
