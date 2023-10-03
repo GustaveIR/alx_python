d9ab53c185c7d3522dc5ff9393872ee7d852922e
GustaveIR <iraguhagustave@gmail.com>
2023-10-03 18:26:24 +0200

    python-object_relational_mapping

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    # Create a cursor object to interact with the database
+    cur = db.cursor()
+
+    # Execute the query to retrieve cities with their corresponding states
+    cur.execute(
+        "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id"
+    )
+
+    # Fetch all the rows that match the query
+    rows = cur.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
