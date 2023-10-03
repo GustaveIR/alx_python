8fa9cbde7fef4dac41ee7ec48eed09cc812c1e10
GustaveIR <iraguhagustave@gmail.com>
2023-10-03 15:51:53 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,31 +11,32 @@ if __name__ == '__main__':
     # Connect to the MySQL server
     db = MySQLdb.connect(
         host='localhost',
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
         port=3306
     )
 
     # Create a cursor object to interact with the database
-    cur = db.cursor()
+    c = db.cursor()
 
-    # Get the state name from the command line argument
-    state_name = argv[4]
 
     # Execute the query to retrieve states matching the provided name
-    cur.execute(
-        "SELECT * FROM states WHERE name=%s ORDER BY id;",
-        (state_name,)
+    c.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' BY id;"
+        .format(sys.argv[4]0)
+        
     )
 
     # Fetch all the rows that match the query
-    rows = cur.fetchall()
+    rows = c.fetchall()
 
     # Print the results
     for row in rows:
         print(row)
 
     # Close the cursor and database connection
-    cur.close()
+    c.close()
     db.close()
+
+   
