8397878d23fb7275211b2fd26337c39cafb8ce14
GustaveIR <iraguhagustave@gmail.com>
2023-10-03 19:17:24 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,10 +6,10 @@ Script that lists all cities from a specific state in the database hbtn_0e_4_usa
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__";
     # Connect to the MySQL server
     db = MySQLdb.connect(
-        host='localhost',
+        host="localhost",
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
@@ -21,24 +21,16 @@ if __name__ == '__main__':
 
     # Execute the query to retrieve cities of the specified state
     cur.execute(
-        "SELECT cities.name "
-        "FROM cities "
-        "JOIN states ON cities.state_id = states.id "
-        "WHERE states.name = %s "
-        "ORDER BY cities.id",
-        (sys.argv[4],)
+        "SELECT cities.name FROM cities INNER JOIN states ON state_id=cities.states_id WHERE states.name = %s ", (sys.argv[4],)
     )
 
     # Fetch all the rows that match the query
     rows = cur.fetchall()
 
     # Print the results
-    if rows:
-        city_names = ', '.join(row[0] for row in rows)
-        print(city_names)
-    else:
-        print("No cities found for the specified state.")
-
+    tmp = list(row[0] for row in rows)
+        
+        print(*tmp, sep=", ")
     # Close the cursor and database connection
     cur.close()
     db.close()
