7ea27a42db2cf50818c7ee158c06e33ff8d90141
GustaveIR <iraguhagustave@gmail.com>
2023-10-05 19:44:15 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -5,26 +5,16 @@ import sys
 if __name__ == '__main__':
     try:
         # Connect to the MySQL server
-        db = MySQLdb.connect(
-            host='localhost',
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            port=3306
-        )
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
         # Create a cursor object to interact with the database
         cur = db.cursor()
 
         # Execute the query to retrieve cities of the specified state
-        cur.execute(
-            "SELECT cities.name "
-            "FROM cities "
-            "JOIN states ON cities.state_id = states.id "
-            "WHERE states.name = %s "
-            "ORDER BY cities.id",
-            (sys.argv[4],)
-        )
+        cur.execute("""SELECT cities.name FROM 
+                       cities INNER JOIN states ON state_id = cities.states.id 
+                       WHERE states.name = %s """, (sys.argv[4],))
 
         # Fetch all the rows that match the query
         rows = cur.fetchall()
