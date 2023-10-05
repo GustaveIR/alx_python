79dd17805d0e62758574ccfcec8ee72c6e12d890
GustaveIR <iraguhagustave@gmail.com>
2023-10-05 19:47:59 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -5,8 +5,10 @@ import sys
 if __name__ == '__main__':
     try:
         # Connect to the MySQL server
-        db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3],port=3306
+        db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
         )
 
         # Create a cursor object to interact with the database
