9dd1df5e27fdfa2b80c9aae934ba711fbf5ed79b
GustaveIR <iraguhagustave@gmail.com>
2023-10-14 09:54:14 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == '__main__':
     try:
-        # Connect to the MySQL server
+    
         db = MySQLdb.connect(
             host='localhost',
             user=sys.argv[1],
@@ -13,10 +13,10 @@ if __name__ == '__main__':
             port=3306
         )
 
-        # Create a cursor object to interact with the database
+        
         cur = db.cursor()
 
-        # Execute the query to retrieve cities of the specified state
+        
         state_name = sys.argv[4]
         cur.execute(
             "SELECT cities.name "
@@ -27,10 +27,10 @@ if __name__ == '__main__':
             (state_name,)
         )
 
-        # Fetch all the rows that match the query
+        
         rows = cur.fetchall()
 
-        # Print the results
+        
         if rows:
             city_names = ', '.join(row[0] for row in rows)
             print(city_names)
@@ -42,6 +42,6 @@ if __name__ == '__main__':
     except Exception as e:
         print("Error: {}".format(e))
     finally:
-        # Close the cursor and database connection
+       
         cur.close()
         db.close()
