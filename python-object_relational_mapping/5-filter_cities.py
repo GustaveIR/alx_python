ad5e5f8e3feba48a9ae92d930ec21e1b0ad8575d
GustaveIR <iraguhagustave@gmail.com>
2023-10-17 15:33:41 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -19,7 +19,12 @@ if __name__ == '__main__':
         
         state_name = sys.argv[4]
         cur.execute(
-            "SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id  WHERE states.name = %s ORDER BY cities.id, (state_name,)"
+            "SELECT cities.name "
+            "FROM cities "
+            "JOIN states ON cities.state_id = states.id "
+            "WHERE states.name = %s "
+            "ORDER BY cities.id",
+            (state_name,)
         )
 
         
