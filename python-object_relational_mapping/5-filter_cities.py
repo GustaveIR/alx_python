ae8e437b21d8fb0dda6f2056b930b75d6f7f4568
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-10-21 15:24:38 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -2,7 +2,14 @@
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
+"""
+Module Documentation: Provide a brief description of what this module does.
+"""
+
+def get_cities_by_state(state_name):
+    """
+    Function Documentation: Describe what this function does.
+    """
     try:
         # Connect to the MySQL server
         db = MySQLdb.connect(
@@ -16,18 +23,15 @@ if __name__ == '__main__':
         # Create a cursor object
         cur = db.cursor()
 
-        # Get the state name from command line arguments
-        state_name = sys.argv[4]
-
         # Execute the SQL query
         cur.execute(
-    "SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') "
-    "FROM cities "
-    "JOIN states ON cities.state_id = states.id "
-    "WHERE states.name = %s "
-    "ORDER BY cities.id",
-    (state_name,)
-)
+            "SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') "
+            "FROM cities "
+            "JOIN states ON cities.state_id = states.id "
+            "WHERE states.name = %s "
+            "ORDER BY cities.id",
+            (state_name,)
+        )
 
         # Fetch the results
         rows = cur.fetchall()
@@ -37,12 +41,20 @@ if __name__ == '__main__':
             city_names = ', '.join(row[0] for row in rows)
             print(city_names)
         else:
-            print("No cities found for the state: {}".format(state_name))
+            print(f"No cities found for the state: {state_name}")
 
     except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
+        print(f"MySQL Error: {e}")
     except Exception as e:
-        print("Error: {}".format(e))
- 
+        print(f"Error: {e}")
+    finally:
         cur.close()
         db.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> <password> <database> <state_name>")
+        sys.exit(1)
+        
+    state_name = sys.argv[4]
+    get_cities_by_state(state_name)
