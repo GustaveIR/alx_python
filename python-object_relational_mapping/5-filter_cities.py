e234a6f553eb23e12935e9083e842f4579ff72cb
Gustave IRAGUHA <40911280+GustaveIR@users.noreply.github.com>
2023-10-25 18:53:24 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -29,43 +29,44 @@ def sort_city_names(city_names):
   return sorted_city_names
 
 def main():
-    database_name = sys.argv[3]
-    username = sys.argv[1]
-    password = sys.argv[2]
-    statename = sys.argv[4]
+  database_name = sys.argv[3]
+  username = sys.argv[1]
+  password = sys.argv[2]
+  statename = sys.argv[4]
 
-    # Connecting to database in the localhost
-    database = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database_name, port=3306)
+  # Connecting to database in the localhost
+  database = MySQLdb.connect(host='localhost', user=username, passwd=password, db=database_name, port=3306)
 
-    # create a cursor
-    cur = database.cursor()
+  # create a cursor
+  cur = database.cursor()
 
-    # using a parameterized query
-    query = "SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE %s = states.name ORDER BY cities.id ASC"
+  # using a parameterized query
+  query = "SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE %s = states.name ORDER BY cities.id ASC"
 
-    # Execute the query with name searched as parameter
-    cur.execute(query, (statename,))
+  # Execute the query with name searched as parameter
+  cur.execute(query, (statename,))
 
-    # obtaining results
-    results = cur.fetchall()
+  # obtaining results
+  results = cur.fetchall()
 
-    # If there are no results, print an empty string
-    if not results:
-        print()
-    else:
-        city_names = ", ".join(row[0] for row in results)
+  # If there are no results, print an empty string
+  if not results:
+    print()
+  else:
+    # Split the city names into a list
+    city_names = [row[0] for row in results]
 
-        # Sort the city names alphabetically
-        city_names = sort_city_names(city_names.split(", "))
+    # Sort the city names alphabetically
+    city_names = sort_city_names(city_names)
 
-        # Print the city names
-        print(", ".join(city_names))
+    # Print the city names
+    print(", ".join(city_names))
 
-    # close cursor
-    cur.close()
+  # close cursor
+  cur.close()
 
-    # close database
-    database.close()
+  # close database
+  database.close()
 
 if __name__ == '__main__':
-    main()
+  main()
