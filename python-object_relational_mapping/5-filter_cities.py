40909df11ecc583b74c1477de6a9f9c667265177
GustaveIR <iraguhagustave@gmail.com>
2023-12-25 18:41:55 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -2,25 +2,25 @@ import MySQLdb
 import sys
 
 def sort_city_names(city_names):
-    """Sorts city names alphabetically, but also groups together city names from the same state."""
     city_names_by_state = {}
-    
+
     for city_name in city_names:
         if "," in city_name:
             state_name = city_name.split(",")[1].strip()
         else:
             state_name = None
-        
+
         if state_name not in city_names_by_state:
             city_names_by_state[state_name] = []
         city_names_by_state[state_name].append(city_name)
-    
+
     sorted_city_names = []
     for state_name, city_names in city_names_by_state.items():
         city_names.sort()  # Sort city names within each state group
-        sorted_city_names += city_names
+        sorted_city_names.extend(city_names)  # Use extend instead of +=
     
     return sorted_city_names
+
 
 
 def main():
