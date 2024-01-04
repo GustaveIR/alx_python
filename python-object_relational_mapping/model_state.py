7e5bb327c9ea7237dffcb69a340a86d2a0f8ef9d
GustaveIR <iraguhagustave@gmail.com>
2024-01-04 19:44:44 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3
-"""Start link class to table in database 
-"""
+"""Start link class to table in database"""
 import sys
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -12,6 +10,19 @@ class State(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
 
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    try:
+        engine = create_engine(f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}",
+                               pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+        print("Table created successfully.")
+    except Exception as e:
+        print("An error occurred:", e)
+        sys.exit(1)
+
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    main()
