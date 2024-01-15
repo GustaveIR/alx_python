7565a848707a54898866b27d1a717b30c670f8e0
GustaveIR <iraguhagustave@gmail.com>
2024-01-15 11:15:26 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/6-model_state.sql
+++ python-object_relational_mapping/6-model_state.sql
@@ -1,8 +1,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa
 USE hbtn_0e_6_usa
 SHOW CREATE TABLE states;
-
 if __name__ == "__main__":
-    from sqlalchemy import create_engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
