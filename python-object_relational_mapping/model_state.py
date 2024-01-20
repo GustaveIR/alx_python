e9c21aa740b3649a41e991a48f28fa2cd1750def
GustaveIR <iraguhagustave@gmail.com>
2024-01-20 17:34:45 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,8 +4,25 @@
 import sys
 from model_state import Base, State
 
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Create an engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Create the table
     Base.metadata.create_all(engine)
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Additional code can be added here for further database operations
+
+    # Close the session
+    session.close()
