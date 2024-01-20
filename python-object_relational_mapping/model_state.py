d92c48e4daf2564b9d7cd8583b4ad30175d0652b
GustaveIR <iraguhagustave@gmail.com>
2024-01-20 17:38:09 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 import sys
 from model_state import Base, State
-
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database_name>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
-    # Create an engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    # Create the table
-    Base.metadata.create_all(engine)
+    try:
+        engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{database}', pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+        print("Tables created successfully.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
 
-    # Create a session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Additional code can be added here for further database operations
-
-    # Close the session
-    session.close()
+if __name__ == "__main__":
+    main()
