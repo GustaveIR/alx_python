efdeb11f7ebb1742620b2ceef85f675fb0635d30
GustaveIR <iraguhagustave@gmail.com>
2024-04-11 20:33:51 +0200

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -31,7 +31,7 @@ def fetch_employee_data(employee_id):
         # Display employee TODO list progress
         print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
         for task in completed_tasks:
-            print(f"\t{task['title']}")  # Format tasks with 1 tabulation and 1 space
+            print(f"\t{task['title']}")  # Format tasks with 1 tabulation
 
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {e}")
